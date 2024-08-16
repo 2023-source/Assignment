d9b3f02a399dc39fd4a934df441e8cb753c3d822
2023-source <122154212+2023-source@users.noreply.github.com>
2024-08-16 12:19:21 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,25 @@ const  express = require('express')
 const  dotenv = require ('dotenv')
 const  mongoose = require ('mongoose')
 const  cors = require('cors')
-// const  cookieParser = require ('cookie-parser')
+const  cookieParser = require ('cookie-parser')
 const  authRoute = require ('./routes/auth.js')
+const  detailRoute = require('./routes/details.js')
 const  classroomRoute = require ('./routes/classroom.js')
 
 
-dotenv.config()
+dotenv.config();
+
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 
 
 const connect = async()=>
 {
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017/Assignment");
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
         console.log('MongoDb connection successful')
     } catch (err) {
         console.log('MongoDb connection failed')
@@ -25,8 +30,9 @@ const connect = async()=>
 // middleware
 app.use(express.json())
 app.use(cors( { origin: 'http://localhost:3000',credentials: true,}))
-// app.use(cookieParser())
+app.use(cookieParser())
 app.use('/api/auth', authRoute)
+app.use('/api/details', detailRoute)
 app.use('/api/classrooms', classroomRoute)
 
 app.listen(port, ()=>
